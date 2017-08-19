f55ce8ccaed273bebe09cbbc4bac80706d50cc71
aquavirgo <jakubguzikowski@o2.pl>
2017-08-19 18:38:57 +0200

    Retrofit and RxJava

--- Retrofit/src/main/java/retrofit/UVService.java
+++ Retrofit/src/main/java/retrofit/UVService.java
@@ -5,7 +5,7 @@ import retrofit2.http.Path;
 
 public interface UVService {
 
-        @GET("uvi/{location}/current.json?appid=7ff6b5867b4336b380c2f5c87c47e00d")
+        @GET("uvi/{location}/current.json?appid=")
         rx.Observable<UVData> getUVData (@Path("location") String pleaceLocation);
     }
 
